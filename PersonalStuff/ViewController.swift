817feb1cceb22897771945d08a8844436392882c
cdeust <cdeust@icloud.com>
2017-02-04 15:36:12 +0100

    Initial Commit

---
+++ PersonalStuff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalStuff
+//
+//  Created by Clément DEUST on 04/02/2017.
+//  Copyright © 2017 cdeust. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
